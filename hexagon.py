999ddc902e87fb5fe898b101b96c49bb69f821f4
Eclipse <Eclips77@users.noreply.github.com>
2025-06-24 16:59:50 +0300

    feat: implement Hexagon class with area and perimeter methods

---
+++ hexagon.py
@@ -0,0 +1,14 @@
+from calculator import Shape
+import math
+
+class Hexagon(Shape):
+    def __init__(self,side):
+        self.side = side
+
+    def get_area(self):
+        area = (3 * math.sqrt(3) * self.side ** 2) / 2
+        return area
+
+
+    def get_primeter(self):
+        return self.side * 6
