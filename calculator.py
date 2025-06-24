a7fc89cfbaacc6bbf13f7fd0ca74e5884260d1d6
Eclipse <Eclips77@users.noreply.github.com>
2025-06-24 15:35:02 +0300

    adding classes

---
+++ calculator.py
@@ -0,0 +1,17 @@
+# shape.py
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    def __init__(self, name):
+        self.name = name
+
+    @abstractmethod
+    def get_area(self):
+        pass
+
+    @abstractmethod
+    def get_perimeter(self):
+        pass
+
+    def __str__(self):
+        return f"{self.name} shape"
