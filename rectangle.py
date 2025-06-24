3c879b6141bdbd77617da9d656719c32da327a96
Eclipse <Eclips77@users.noreply.github.com>
2025-06-24 15:43:11 +0300

    adding rectangle class

---
+++ rectangle.py
@@ -0,0 +1,17 @@
+from calculator import Shape
+
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        super().__init__("Rectangle")
+        self.width = width
+        self.height = height
+
+    def get_area(self):
+        return self.width * self.height
+
+    def get_perimeter(self):
+        return 2 * (self.width + self.height)
+
+    def __str__(self):
+        return f"{self.name} with width {self.width} and height {self.height}"
